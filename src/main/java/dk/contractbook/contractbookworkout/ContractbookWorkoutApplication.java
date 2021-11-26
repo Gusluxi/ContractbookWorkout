fbf3e380150b8bb66fbe3e1906b0ae38c39a08e0
Gustav <gullegustav@gmail.com>
2021-11-26 13:18:59 +0100

    Initial commit

---
+++ src/main/java/dk/contractbook/contractbookworkout/ContractbookWorkoutApplication.java
@@ -0,0 +1,13 @@
+package dk.contractbook.contractbookworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractbookWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractbookWorkoutApplication.class, args);
+    }
+
+}
