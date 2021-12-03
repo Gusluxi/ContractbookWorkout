36269272590798ca8c1bfb2b31eb2661f6c38baa
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-12-03 14:26:52 +0100

    RestControllers

--- src/main/java/dk/contractbook/contractbookworkout/controllers/Workouts.java
+++ src/main/java/dk/contractbook/contractbookworkout/controllers/Workouts.java
@@ -1,4 +1,59 @@
 package dk.contractbook.contractbookworkout.controllers;
 
+import dk.contractbook.contractbookworkout.models.Workout;
+import dk.contractbook.contractbookworkout.repositories.WorkoutsRepo;
+import org.hibernate.jdbc.Work;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class Workouts {
+
+    @Autowired
+    WorkoutsRepo workouts;
+
+
+    @GetMapping("/workouts")
+    public Iterable<Workout> getWorkout() {
+        return workouts.findAll();
+    }
+
+    @GetMapping("/workouts/{id}")
+    public Workout getWorkout(@PathVariable Long id) {
+        return workouts.findById(id).get();
+    }
+
+    @PostMapping("/workouts")
+    public Workout addWorkout(@RequestBody Workout newWorkout) {
+        newWorkout.setId(null);
+        return workouts.save(newWorkout);
+    }
+
+    @PutMapping("/workouts/{id}")
+    public String updateWorkout(@PathVariable Long id, @RequestBody Workout workoutsToUpdate){
+        if(workouts.existsById(id)) {
+            workoutsToUpdate.setId(id);
+            workouts.save(workoutsToUpdate);
+            return "Workout was updated";
+        } else {
+            return "Workout not found";
+        }
+    }
+
+    @PatchMapping("/workouts/{id}")
+    public String patchWorkout(@PathVariable Long id, @RequestBody Workout workoutsToUpdate) {
+        return workouts.findById(id).map( foundWorkout -> {
+            if(workoutsToUpdate.getWorkoutsId() != null) foundWorkout.setWorkoutId(workoutsToUpdate.getName());
+            if(workoutsToUpdate.getPuuId() != null) foundWorkout.setPuuId(workoutsToUpdate.getPuuId());
+            if(workoutsToUpdate.getName() != null) foundWorkout.setName(workoutsToUpdate.getName());
+            workouts.save(foundWorkout);
+            return "Workout updated";
+        }).orElse("Workout not found");
+    }
+
+    @DeleteMapping("/workouts/{id}")
+    public void deleteSummoner(@PathVariable Long id) {
+        workouts.deleteById(id);
+    }
+
 }
