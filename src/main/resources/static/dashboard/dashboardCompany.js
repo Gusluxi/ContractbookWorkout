b31f028a9049915aa8fceb03be49c9a42e476057
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-12-03 16:41:03 +0100

    Js

--- src/main/resources/static/dashboard/dashboardCompany.js
+++ src/main/resources/static/dashboard/dashboardCompany.js
@@ -1,36 +1,17 @@
-const companyWorkoutWrapperMonthly = document.getElementById("company-workout-monthly");
 const companyWorkoutWrapperYearly = document.getElementById("company-workout-yearly");
-let date = new Date();
-let startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-let endDate = new Date(date.getFullYear(), date.getMonth() +1, 0);
+const companyWorkoutWrapperMonthly = document.getElementById("company-workout-monthly");
+
 console.log(startDate + " & " + endDate);
 let fetchedWorkouts;
 
 fetch(APIUrl + "/workouts")
     .then(response => response.json())
     .then(workouts => {
-        workouts.map(addWorkOutInfoToDivList);
         fetchedWorkouts = workouts;
-        createCompanyWorkoutCardYearly(workouts);
+        displayYearlyWorkouts(workouts);
+        displayMonthlyWorkouts(workouts);
 
     });
-
-fetch(APIUrl + "/workouts")
-    .then(response => response.json())
-    .then(workouts => {
-        workouts.map(createCompanyWorkoutCardMonthly);
-    });
-
-
-function addWorkOutInfoToDivList(workout){
-    const workoutInfoToDiv = document.createElement("div");
-    companyWorkoutWrapperYearly.appendChild(workoutInfoToDiv);
-    console.log(workout);
-    console.log(workoutInfoToDiv);
-    console.log("hej");
-
-    displayYearlyWorkouts(workoutInfoToDiv, workout);
-}
 
 
 function displayYearlyWorkouts(workouts) {
@@ -44,3 +25,17 @@ function displayYearlyWorkouts(workouts) {
     `;
     companyWorkoutWrapperYearly.appendChild(yearlyWorkoutsDiv);
 }
+
+function displayMonthlyWorkouts(workouts) {
+    const monthlyWorkoutsDiv = document.createElement("div");
+    const monthlyWorkouts = workouts.filter(workouts => {
+        let workoutDates = new Date(workouts.workoutDates);
+        return (startDate < workoutDates && workoutDates < endDate)
+    });
+    monthlyWorkoutsDiv.innerHTML = `
+    <div>${monthlyWorkouts.length}</div>
+    `;
+    companyWorkoutWrapperMonthly.appendChild(monthlyWorkoutsDiv);
+}
+
+
