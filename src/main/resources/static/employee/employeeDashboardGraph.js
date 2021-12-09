d8a283b8d2509d578a35ca3b6a25b1f70a7d7096
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-12-09 16:32:03 +0100

    Employee graph, lines, counter, whatever

--- src/main/resources/static/employee/employeeDashboardGraph.js
+++ src/main/resources/static/employee/employeeDashboardGraph.js
@@ -17,21 +17,65 @@ fetch(APIUrl + "/employees/" + employeeId )
             .then(response => response.json())
             .then(employeeWorkouts => {
                 console.log(employeeWorkouts);
-                constructWorkoutGraph();
+                constructWorkoutGraph(employeeWorkouts);
             })
     })
 
-const employeeDashboardGraphDiv = document.getElementById("employee-workout-dates");
+const employeeDashboardDateDiv = document.getElementById("employee-workout-dates");
+const employeeDashboardGraphDiv = document.getElementById("employee-workout-graph");
+const employeeDashboardCounterDiv = document.getElementById("employee-workout-counter");
 
 
-function constructWorkoutGraph() {
+
+function constructWorkoutGraph(employeeWorkouts) {
+    const graphCountDiv = document.createElement("div");
+    graphCountDiv.innerHTML = `
+    <div id="graph-counter">
+    <a>25</a>
+    <a>20</a>
+    <a>15</a>
+    <a>10</a>
+    <a>5</a>
+    </div>
+    `;
+    employeeDashboardCounterDiv.appendChild(graphCountDiv);
+
+    let graphBarHeight = 0;
     for (let d = startDate; d <= endDate; d.setDate(d.getDate()+1)) {
         const workoutGraphDateDiv = document.createElement("div");
         workoutGraphDateDiv.setAttribute("id", "date-wrapper");
-        console.log(d);
         workoutGraphDateDiv.innerHTML = `
         <div class="date">${d.getDate() + "/" + (d.getMonth()+1)}</div>
         `;
-        employeeDashboardGraphDiv.appendChild(workoutGraphDateDiv);
+        employeeDashboardDateDiv.appendChild(workoutGraphDateDiv);
+        const workoutGraphBarDiv = document.createElement("div");
+        workoutGraphBarDiv.setAttribute("id","bar-wrapper");
+
+        employeeWorkouts.map(workout => {
+            if (new Date(workout.workoutDate).toDateString() === d.toDateString()) {
+                graphBarHeight += 20.5;
+                console.log(graphBarHeight);
+                console.log(d)
+            }
+        })
+        workoutGraphBarDiv.innerHTML = `
+        <div id="bar-${d.toDateString()}" style="height: ${graphBarHeight}px; width: 28px; margin-left: 6px; background-color: cornflowerblue;">
+        `;
+        employeeDashboardGraphDiv.appendChild(workoutGraphBarDiv);
     }
+
+    const graphLineDiv = document.createElement("div");
+    graphLineDiv.setAttribute("id", "line-wrapper");
+    graphLineDiv.innerHTML = `
+    <div class="graph-line"></div>
+    <div class="graph-line"></div>
+    <div class="graph-line"></div>
+    <div class="graph-line"></div>
+    <div class="graph-line"></div>
+    `;
+    employeeDashboardGraphDiv.appendChild(graphLineDiv);
+
+
+
 }
+
